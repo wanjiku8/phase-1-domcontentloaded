4480792a61ba7a4ddef932b8698c669e110e260f
Faith Wanjiku <wanjikuf141@gmail.com>
2024-12-08 02:39:24 +0300

    commit

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("The DOM has loaded");
+  
+    const paragraph = document.getElementById("text");
+  
+    paragraph.textContent = "This is really cool!";
+  });
+  
+  console.log(
+    "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+  );
+  
